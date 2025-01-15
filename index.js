fbd6a549e0137b33e86ce658364ac6cd0b6e1900
Berkehan Gokturk <berkehan7119@gmail.com>
2025-01-15 17:46:56 +0300

    first commit

--- index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const openWindow = document.getElementById('open-window');
-    const btnWindow = document.getElementById('btn-window');
-
-    btnWindow.addEventListener('click', (e) => {
-        e.preventDefault();
-        openWindow.style.display = 'flex';
-    });
-});
